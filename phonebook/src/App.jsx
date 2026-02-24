ee6c6a2ddb3bb98ed83ef406e76df1f179586c67
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-24 17:55:20 +0530

    feat: implement dynamic name filtering for phonebook (exercise 2.9)

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const Person = (props) => {
-  console.log("Person props", props);
+  //console.log("Person props", props);
   return (
     <>
       <p>{props.name} {props.number}</p>
@@ -11,10 +11,14 @@ const Person = (props) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '123456789' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('');
+  const [newFilter, setNewFilter] = useState('');
 
   const addName = (event) => {
     event.preventDefault()
@@ -41,9 +45,26 @@ const App = () => {
     setNewNumber(event.target.value);
   }
 
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value);
+  }
+
+
+  const personsToShow = newFilter === '' ? persons : persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()));
+
+  //console.log("Filtered persons:", personsToShow);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          Filter shown names:
+          <input value={newFilter}
+            onChange={handleFilterChange}
+          />
+        </div>
+        <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name:
@@ -61,7 +82,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person =>
+      {personsToShow.map(person =>
         <Person key={person.name} name={person.name} number={person.number} />
       )}
     </div>
