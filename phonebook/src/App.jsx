0ed6d03a8c752dea1bdc1ee0c5d36d7d755e5a68
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-02-24 16:51:28 +0530

    feat: add Person component and append new name to persons array (exercise 2.6)

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,24 +1,50 @@
 import { useState } from 'react'
 
+const Person = (props) => {
+  console.log("Person props", props);
+  return(
+    <>
+    <p>{props.name}</p>
+    </>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
-  ]) 
+  ])
   const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('add btn clicked', event.target)
+    console.log('new name is :',newName)
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName}
+            onChange={handleNameChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+        {persons.map(person =>
+          <Person key={person.name} name={person.name} />
+        )}
     </div>
   )
 }
